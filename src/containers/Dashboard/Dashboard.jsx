c79c0ee54672ed6b0f46b16db95a794efe18aef2
George McCann <george_mccann7@hotmail.com>
2023-09-24 22:21:45 +0100

    Basic dashboard coming soon text

--- src/containers/Dashboard/Dashboard.jsx
+++ src/containers/Dashboard/Dashboard.jsx
@@ -2,6 +2,10 @@ import React from "react";
 
 export default function Dashboard() { 
   return (
-     <h1>Home</h1>
+    <div class="row justify-content-center align-items-center">
+      <div class="col-md-6 text-center py-5">
+        <h1>coming soon!</h1>
+      </div>
+    </div>
   );
 }
