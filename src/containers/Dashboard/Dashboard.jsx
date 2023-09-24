60e39c0f3feb623d85c43ac2bfcc7d17602a5f3f
George McCann <george_mccann7@hotmail.com>
2023-09-24 22:42:20 +0100

    Fix class name

--- src/containers/Dashboard/Dashboard.jsx
+++ src/containers/Dashboard/Dashboard.jsx
@@ -2,8 +2,8 @@ import React from "react";
 
 export default function Dashboard() { 
   return (
-    <div class="row justify-content-center align-items-center">
-      <div class="col-md-6 text-center py-5">
+    <div className="row justify-content-center align-items-center">
+      <div className="col-md-6 text-center py-5">
         <h1>coming soon!</h1>
       </div>
     </div>
